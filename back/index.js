b0d49ae4a49b3adf7ce34e5a58c54b3299054ae7
critanaro <critanaro@gmail.com>
2020-09-19 15:47:49 -0500

    updated node server

--- back/index.js
+++ back/index.js
@@ -12,10 +12,10 @@ app.use(cors());
 
 //use this to change port (5000 for testing)
 
-const PORT = 5000;
-log.info('index.js is working');
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => log.info(`The server has started on port: ${PORT}`));
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+
 /*
 mongoose.connect("mongodb+srv://critanaro:babLTAy1Y5a0OEQS@main.w3oxl.mongodb.net/test?retryWrites=true&w=majority",
     {
